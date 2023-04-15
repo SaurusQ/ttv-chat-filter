afc4dddbdf2460db342ec178f663322ada153ffe
SaurusQ <53377378+SaurusQ@users.noreply.github.com>
2023-04-15 17:49:56 +0300

    added start of auto claim

--- popup/popup.js
+++ popup/popup.js
@@ -3,11 +3,12 @@ document.getElementById('enableFilter').addEventListener('change', (event) => {
   const enabled = event.target.checked
   chrome.storage.sync.set({ enableFilter: enabled }, function() {
     console.log('Extension ' + (enabled ? 'enabled' : 'disabled'))
-  });
+  })
   
   chrome.tabs.query({}, tabs => {
     tabs.forEach(tab => {
       chrome.tabs.sendMessage(tab.id, {type: 'enableFilter', value: enabled})
-    });
+        .catch(err => {})
+    })
   })
-});
+})
