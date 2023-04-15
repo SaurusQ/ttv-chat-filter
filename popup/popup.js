559f413e276af0a2f0bf2c71fbf5a2314631076c
SaurusQ <313.saurus@gmail.com>
2023-04-15 13:41:35 +0300

    working popup toggle

---
+++ popup/popup.js
@@ -0,0 +1,13 @@
+
+document.getElementById('enableFilter').addEventListener('change', (event) => {
+  const enabled = event.target.checked
+  chrome.storage.sync.set({ enableFilter: enabled }, function() {
+    console.log('Extension ' + (enabled ? 'enabled' : 'disabled'))
+  });
+  
+  chrome.tabs.query({}, tabs => {
+    tabs.forEach(tab => {
+      chrome.tabs.sendMessage(tab.id, {type: 'enableFilter', value: enabled})
+    });
+  })
+});
--- scripts/chat_filter.js
+++ scripts/chat_filter.js
@@ -1,4 +1,11 @@
 
+var chatObserver
+var chatElement
+const chatObserverOptions = {
+  childList: true,
+  subtree: true
+};
+
 function filterMsg(msg) {
   /*if(msg.length <= 4) return true
   msg = msg.toLowerCase()
@@ -8,38 +15,84 @@ function filterMsg(msg) {
   return regex.test(msg)
 }
 
-window.onload = function() {
-  const chatInput = document.querySelector('div[class$="chat-input"]')
-  const chatInputDiv = chatInput.querySelector('div')
-
-  const chatElement = document.querySelector('div[data-test-selector="chat-scrollable-area__message-container"]');
- 
-  const chatObserver = new MutationObserver((mutationsList) => {
-    for (const mutation of mutationsList) {
-      if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-        //console.log(mutation)
-        for(const addedNode of mutation.addedNodes) {
-          if (Object.prototype.toString.call(addedNode) !== '[object HTMLDivElement]') {
-            console.log("break")
-            break
-          }
-          const messageSpan = addedNode.querySelector('span[data-a-target="chat-message-text"]');
-          if (messageSpan && filterMsg(messageSpan.textContent)) {
-            console.log("Removed: " + messageSpan.textContent)
-            addedNode.style.display = 'none';
-            //chatInputDiv.appendChild(addedNode)
-          }
-          
-          
+const chatCallback = (mutationsList) => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+      //console.log(mutation)
+      for(const addedNode of mutation.addedNodes) {
+        if (Object.prototype.toString.call(addedNode) !== '[object HTMLDivElement]') {
+          //console.log("break")
+          break
+        }
+        const messageSpan = addedNode.querySelector('span[data-a-target="chat-message-text"]');
+        if (messageSpan && filterMsg(messageSpan.textContent)) {
+          console.log("Removed: " + messageSpan.textContent)
+          addedNode.style.display = 'none';
+          //chatInputDiv.appendChild(addedNode)
         }
       }
     }
-  });
-    
-  const chatObserverOptions = {
-    childList: true,
-    subtree: true
-  };
-  chatObserver.observe(chatElement, chatObserverOptions);
+  }
+}
 
+var enabled = true
+chrome.storage.sync.get({ enableFilter: true }, (items) => {
+  enabled = items.enableFilter
+  console.log("enable start: " + enabled)
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log("addListener")
+  if (request.type === 'enableFilter') {
+    enabled = request.value;
+    if (chatObserver)
+    {
+      if (enabled) {
+        if (chatObserver.takeRecords().length === 0 && chatObserver && chatElement) {
+          console.log("observer")
+          chatObserver.observe(chatElement, chatObserverOptions)
+        }
+      }
+      else {
+        console.log("disconnect")
+        chatObserver.disconnect()
+      }
+    }
+    console.log('Extension ' + (enabled ? 'enabled' : 'disabled') + '.');
+  }
+});
+
+window.onload = function() {
+  //if (!enabled) return
+
+  //const chatInput = document.querySelector('div[class$="chat-input"]')
+  //const chatInputDiv = chatInput.querySelector('div')
+
+  var popup = document.createElement('div');
+  popup.id = 'myPopup';
+  popup.style.position = 'fixed';
+  popup.style.bottom = '0';
+  popup.style.left = '0';
+  popup.style.width = '200px';
+  popup.style.height = '100px';
+  popup.style.backgroundColor = 'white';
+  popup.style.border = '1px solid black';
+  popup.style.visibility = 'hidden'; // Initially hide the popup
+
+  // Add content to the popup
+  popup.innerHTML = 'Hello, world!';
+
+  // Append the popup to the document's body
+  document.body.appendChild(popup);
+
+  // Show the popup
+  popup.style.visibility = 'visible';
+
+  chatElement = document.querySelector('div[data-test-selector="chat-scrollable-area__message-container"]');
+  chatObserver = new MutationObserver(chatCallback);
+    
+  if (enabled) {
+    chatObserver.observe(chatElement, chatObserverOptions)
+    console.log("observer")
+  }
 };
