1dc649c9716fcb93ad1dc4db4848eaed75009e54
SaurusQ <313.saurus@gmail.com>
2023-04-15 17:49:56 +0300

    added start of auto claim

--- scripts/chat_filter.js
+++ scripts/chat_filter.js
@@ -75,33 +75,30 @@ chrome.storage.sync.get({ enableFilter: true }, (items) => {
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log("addListener")
   if (request.type === 'enableFilter') {
     enabled = request.value;
     if (chatObserver)
     {
       if (enabled) {
         if (chatObserver.takeRecords().length === 0 && chatObserver && chatElement) {
-          console.log("observer")
           chatObserver.observe(chatElement, chatObserverOptions)
         }
       }
       else {
-        console.log("disconnect")
         chatObserver.disconnect()
       }
     }
-    console.log('Extension ' + (enabled ? 'enabled' : 'disabled') + '.');
+    console.log('Filter ' + (enabled ? 'enabled' : 'disabled'));
   }
 });
 
-window.onload = function() {
+window.addEventListener("load", () => {
   //if (!enabled) return
 
   //const chatInput = document.querySelector('div[class$="chat-input"]')
   //const chatInputDiv = chatInput.querySelector('div')
 
-  var popup = document.createElement('div');
+  /*var popup = document.createElement('div');
   popup.id = 'myPopup';
   popup.style.position = 'fixed';
   popup.style.bottom = '0';
@@ -113,19 +110,17 @@ window.onload = function() {
   popup.style.visibility = 'hidden'; // Initially hide the popup
 
   // Add content to the popup
-  popup.innerHTML = 'Hello, world!';
+  popup.innerHTML = 'Hello, world!'
 
   // Append the popup to the document's body
-  document.body.appendChild(popup);
+  document.body.appendChild(popup)
 
   // Show the popup
-  popup.style.visibility = 'visible';
+  popup.style.visibility = 'visible'*/
 
-  chatElement = document.querySelector('div[data-test-selector="chat-scrollable-area__message-container"]');
-  chatObserver = new MutationObserver(chatCallback);
-    
+  chatElement = document.querySelector('div[data-test-selector="chat-scrollable-area__message-container"]')
+  chatObserver = new MutationObserver(chatCallback)
   if (enabled) {
     chatObserver.observe(chatElement, chatObserverOptions)
-    console.log("observer")
   }
-};
+})
