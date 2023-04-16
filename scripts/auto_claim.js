f0f047df8f43a793d14e13fb80ab6f2939d25f8a
SaurusQ <313.saurus@gmail.com>
2023-04-16 19:27:37 +0300

    autoclaim working

--- scripts/auto_claim.js
+++ scripts/auto_claim.js
@@ -1,35 +1,21 @@
 
-const claimCallback = (mutationsList) => {
-  for (const mutation of mutationsList) {
-    console.log("MUTATION")
-    console.log(mutation)
-    /*if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-      //console.log(mutation)
-      for(const addedNode of mutation.addedNodes) {
-        if (Object.prototype.toString.call(addedNode) !== '[object HTMLDivElement]') {
-          //console.log("break")
-          break
-        }
-        const messageSpan = addedNode.querySelector('span[data-a-target="chat-message-text"]');
-        if (messageSpan && filterMsg(messageSpan.textContent)) {
-          console.log("Removed: " + messageSpan.textContent)
-          addedNode.style.display = 'none';
-          //chatInputDiv.appendChild(addedNode)
-        }
-      }
-    }*/
-  }
+const claimCallback = (mutations) => {
+  mutations.forEach(mutation => {
+    const button = mutation.target.querySelector('button[aria-label="Claim Bonus"]')
+    if (button) {
+      button.click()
+      console.log("Watch reward claimed!")
+    }
+  })
 }
 
 window.addEventListener("load", () => {
-  console.log("autoclaim start")
   const claimObserverOptions = {
     childList: true,
     subtree: true
-  };
-  const claimBase = document.querySelector('div[class="ScTransitionBase-sc-hx4quq-0 gSiBL tw-transition"]')
-  console.log(claimBase)
-  const claimObserver = new MutationObserver(claimCallback);
-  console.log(claimObserver)
+  }
+  const claimBase = document.querySelector('div[data-test-selector="chat-input-buttons-container"]')
+  const claimObserver = new MutationObserver(claimCallback)
   claimObserver.observe(claimBase, claimObserverOptions)
+  console.log("autoclaim enabled")
 })
